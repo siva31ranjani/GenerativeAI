58acda2908c04179620b09bdc62ec311536a2b8f
Sivaranjani S <137415013+siva31ranjani@users.noreply.github.com>
2025-07-04 06:14:54 +0530

    Update app

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash",
                              temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
